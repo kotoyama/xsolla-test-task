25f55307193fa066c42b11ae2c249d8f82e6a3c5
kotoyama <kotoyama@list.ru>
2021-06-22 00:36:41 +0500

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import-helpers'],
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    eqeqeq: ['warn'],
+    'new-cap': 'off',
+    'no-shadow': 'off',
+    'no-console': ['warn'],
+    'no-return-await': 'off',
+    'import/extensions': 'off',
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
+    'lines-between-class-members': 'off',
+    'max-classes-per-file': ['error', 2],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-underscore-dangle': ['warn', { allow: ['_id'] }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+}
